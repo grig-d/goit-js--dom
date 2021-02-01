5d65b07dffe7ccf6a38ccf7074038ac69e8b7a83
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-02-01 02:52:22 +0200

    wip

--- js/m7-14-modal.js
+++ js/m7-14-modal.js
@@ -1,9 +1,35 @@
-const boxRef = document.querySelector('.js-box');
-boxRef.addEventListener('mouseenter', event => {
-  event.target.classList.add('box--active');
-  console.log(event);
-});
-boxRef.addEventListener('mouseleave', event => {
-  event.target.classList.remove('box--active');
-  console.log(event);
-});
+// чтобы показать модалку достаточно по клику кнопки добавить класс show-modal в тег body
+const openModalBtn = document.querySelector('button[data-action="open-modal"]');
+openModalBtn.addEventListener('click', openModalHandler);
+
+const closeModalBtn = document.querySelector(
+  'button[data-action="close-modal"]',
+);
+closeModalBtn.addEventListener('click', closeModalHandler);
+
+// модалку надо закрывать не только кнопкой, но и кликом за пределами модалки
+// для этого вешаем слушатель на backdrop (не на window)
+const backdropRef = document.querySelector('.js-backdrop');
+backdropRef.addEventListener('click', closeModalByBackdropHandler);
+
+function openModalHandler() {
+  // закрытие по нажатию escape
+  window.addEventListener('keydown', pressEscapeHandler);
+  document.body.classList.add('show-modal');
+}
+function closeModalHandler() {
+  window.removeEventListener('keydown', pressEscapeHandler);
+  document.body.classList.remove('show-modal');
+}
+function closeModalByBackdropHandler(event) {
+  if (event.target === event.currentTarget) {
+    closeModalHandler();
+  }
+}
+
+function pressEscapeHandler(event) {
+  if (event.code === 'Escape') {
+    closeModalHandler();
+    console.log('stalker');
+  }
+}
