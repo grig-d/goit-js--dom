eff2b61a03e7ae33efa19d68dba96f36cb133754
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-02-06 01:55:33 +0200

    wip

--- js/m7-14-mouse.js
+++ js/m7-14-mouse.js
@@ -1,9 +1,21 @@
+// mouseenter mouseleave
 const boxRef = document.querySelector('.js-box');
 boxRef.addEventListener('mouseenter', event => {
   event.target.classList.add('box--active');
   console.log(event);
 });
 boxRef.addEventListener('mouseleave', event => {
+  event.target.classList.remove('box--active');
+  console.log(event);
+});
+
+// mouseover mouseout 
+const box2Ref = document.querySelector('.box2.js-box');
+box2Ref.addEventListener('mouseover', event => {
+  event.target.classList.add('box--active');
+  console.log(event);
+});
+box2Ref.addEventListener('mouseout', event => {
   event.target.classList.remove('box--active');
   console.log(event);
 });
