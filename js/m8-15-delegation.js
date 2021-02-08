b8165f59c101533ad5fe435ae6e58d6587ebabce
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-02-08 23:55:05 +0200

    wip

--- js/m8-15-delegation.js
+++ js/m8-15-delegation.js
@@ -1,18 +1,34 @@
 const refs = {
   tags: document.querySelector('.js-tags'),
+  activeTagOutput: document.querySelector('.js-active-tag'),
 };
 
 refs.tags.addEventListener('click', onTagsClick);
+
 function onTagsClick(event) {
   if (event.target.nodeName !== 'BUTTON') {
     return;
   }
-  const currentActiveTag = event.currentTarget.querySelector(
-    '.tags__btn--active',
-  );
+  setActiveTag(event.target);
+  updateOutput(event.target.dataset.value);
+}
+
+function setActiveTag(nextActiveTag) {
+  const currentActiveTag = refs.tags.querySelector('.tags__btn--active');
+  if (nextActiveTag === currentActiveTag) {
+    currentActiveTag.classList.remove('tags__btn--active');
+    return;
+  }
   if (currentActiveTag) {
     currentActiveTag.classList.remove('tags__btn--active');
   }
-  const nextActiveTag = event.target;
   nextActiveTag.classList.add('tags__btn--active');
+}
+
+function updateOutput(value) {
+  if (refs.activeTagOutput.textContent === value) {
+    refs.activeTagOutput.textContent = '';
+    return;
+  }
+  refs.activeTagOutput.textContent = value;
 }
