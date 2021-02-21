2e72ad4a07ad5ff3fce22b970fd95103f069d6aa
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-02-21 02:32:05 +0200

    terminalcommit

--- js/m10-20-templating.js
+++ js/m10-20-templating.js
@@ -1,8 +1,26 @@
+// insertAdjacentHTML
+
 const tech = ['HTML', 'CSS', 'JS', 'React'];
 
 const galleryRef = document.querySelector('.js-gallery');
 
-const markup = tech.map(element => `<li><a href="">${element}</a></li>`).join('');
+const markup = tech
+  .map(element => `<li><a href="">${element}</a></li>`)
+  .join('');
 console.log(markup);
 
 galleryRef.insertAdjacentHTML('beforeend', markup);
+
+// oldSchool handlebarsjs
+
+console.log(Handlebars); // Handlebars добавлено на window
+
+// получаем внутреннее содержимое в виде <li>{{label}}</li>
+const html = document.querySelector('#list-item').innerHTML.trim();
+
+const template = Handlebars.compile(html); // создаётся функция
+
+const markupHandlebars = template({ items: ['HTML', 'CSS', 'JS', 'React'] });
+
+const galleryHandlebarsRef = document.querySelector('.js-gallery-handlebars');
+galleryHandlebarsRef.insertAdjacentHTML('beforeend', markupHandlebars);
