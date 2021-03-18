d3fa449332272ccee1e257588b68a8a2a0d5a481
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-03-18 17:10:38 +0200

    wip

--- js/m8-goit-js-hw-08-gallery-js28.js
+++ js/m8-goit-js-hw-08-gallery-js28.js
@@ -3,6 +3,9 @@ import galleryItems from './gallery-items.js';
 const refs = {
   galleryList: document.querySelector('.js-gallery'),
   lightbox: document.querySelector('.js-lightbox'),
+  lightboxImage: document.querySelector('img.lightbox__image'),
+  closeLightbox: document.querySelector('button[data-action="close-lightbox"]'),
+  overlay: document.querySelector('div.lightbox__overlay'),
 };
 
 const galleryMarkup = galleryItems.map(object => {
@@ -28,29 +31,31 @@ let urlValue;
 refs.galleryList.addEventListener('click', event => {
   event.preventDefault();
   if (event.currentTarget !== event.target) {
-      urlValue = event.target.dataset.source;
-      refs.lightbox.classList.add('is-open');
+    urlValue = event.target.dataset.source;
+    refs.lightboxImage.src = urlValue;
+    console.log(urlValue);
+    refs.lightbox.classList.add('is-open');
+    window.addEventListener('keydown', closeLightbox);
   }
 });
 
+function closeLightbox() {
+  refs.lightbox.classList.remove('is-open');
+  refs.lightboxImage.src = '';
+  window.removeEventListener('keydown', closeLightbox);
+}
+
+refs.closeLightbox.addEventListener('click', closeLightbox);
+
+refs.overlay.addEventListener('click', closeLightbox);
+
+//
+
+window.addEventListener('keydown', () => console.log(event));
+
+// ArrowLeft ArrowRight
 
 
-
-
-
-// Подмена значения атрибута src элемента img.lightbox__image.
-
-// Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
-
-// Очистка значения атрибута src элемента img.lightbox__image.
-// Это необходимо для того, чтобы при следующем открытии модального окна,
-// пока грузится изображение, мы не видели предыдущее.
-
-// Ссылка на оригинальное изображение должна храниться в data-атрибуте source на элементе img,
-// и указываться в href ссылки(это необходимо для доступности).
-
-// Закрытие модального окна по клику на div.lightbox__overlay
-// Закрытие модального окна по нажатию клавиши ESC
 // Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо"
 
 // Task ------------------------------------------------------------------------
@@ -75,3 +80,18 @@ refs.galleryList.addEventListener('click', event => {
 // Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
 
 // Открытие модального окна по клику на элементе галереи.
+
+// Подмена значения атрибута src элемента img.lightbox__image.
+
+// Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
+
+// Очистка значения атрибута src элемента img.lightbox__image.
+// Это необходимо для того, чтобы при следующем открытии модального окна,
+// пока грузится изображение, мы не видели предыдущее.
+
+// Ссылка на оригинальное изображение должна храниться в data-атрибуте source на элементе img,
+// и указываться в href ссылки(это необходимо для доступности).
+
+// Закрытие модального окна по клику на div.lightbox__overlay
+
+// Закрытие модального окна по нажатию клавиши ESC
