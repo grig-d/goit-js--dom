afc6ea94e60fff695d126ca1517220b1048f605b
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-04-02 02:00:42 +0300

    gsap done

--- js/m10-19-gsap-by-venik.js
+++ js/m10-19-gsap-by-venik.js
@@ -1,16 +1,33 @@
 const burgerRef = document.querySelector('.burger');
 const sideBarRef = document.querySelector('.side-bar');
 const closeButtonRef = sideBarRef.querySelector('.x-mark');
+const sideBarLinksRef = sideBarRef.querySelectorAll('.side-bar__link');
 
 const openSideBar = () => {
-  // sideBarRef.classList.add('active');
-  gsap.to(sideBarRef, {x: 0, duration: 0.4});
+  // sideBarRef.classList.add('active'); // don't need if we use gsap
+  gsap.set(sideBarLinksRef, { x: -20, opacity: 0 });
+  const timeline = gsap.timeline();
+  timeline.to(sideBarRef, { x: 0, duration: 0.4 }).to(sideBarLinksRef, {
+    x: 0,
+    opacity: 1,
+    duration: 0.1,
+    stagger: 0.1,
+    ease: 'elastic',
+  });
   window.addEventListener('keydown', closeOnEscape);
 };
 
 const closeSideBar = () => {
-  // sideBarRef.classList.remove('active');
-  gsap.to(sideBarRef, { x: '-100%', duration: 0.2 });
+  // sideBarRef.classList.remove('active'); // don't need if we use gsap
+  const timeline = gsap.timeline();
+  timeline
+    .to(sideBarLinksRef, {
+      y: 25,
+      opacity: 0,
+      duration: 0.4,
+      stagger: -0.1,
+    })
+    .to(sideBarRef, { x: '-100%', duration: 0.4 }, '-=0.4');
   window.removeEventListener('keydown', closeOnEscape);
 };
 
@@ -24,4 +41,4 @@ const closeOnEscape = event => {
 burgerRef.addEventListener('click', openSideBar);
 closeButtonRef.addEventListener('click', closeSideBar);
 
-// time 1-45-00
+// 1-55
