704890515bd01cb240b3971b2b6de5cb077cf986
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-04-07 01:57:10 +0300

    bootstrap.native begin

--- js/m11-21-async-await-subscription.js
+++ js/m11-21-async-await-subscription.js
@@ -1,16 +1,31 @@
-// subscription modal window
+// let promptCounter = 0;
+// let hasSubscribed = false;
+
+// const intervalId = setInterval(() => {
+//   if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubscribed) {
+//     clearInterval(intervalId);
+//     return;
+//   }
+//   console.log('Подпишись на рассылку!');
+//   promptCounter++;
+// }, PROMPT_DELAY);
+
+const modal = new BSN.Modal('#subscriptionModal');
+const refs = {
+  modal: document.querySelector('#subscriptionModal'),
+  // closeSecondaryBtnRef: document.querySelector('.js-secondary-close'),
+};
 
 const PROMPT_DELAY = 1000;
 const MAX_PROMPT_ATTEMPTS = 3;
 
-let promptCounter = 0;
-let hasSubscribed = false;
-
-const intervalId = setInterval(() => {
-  if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubscribed) {
-    clearInterval(intervalId);
-    return;
-  }
-  console.log('Подпишись на рассылку!');
-  promptCounter++;
+setTimeout(() => {
+  modal.show();
 }, PROMPT_DELAY);
+
+// closeSecondaryBtnRef.addEventListener('click', modal.hide);
+
+// кастомное событие custom event из библиотеки bootstrap.native
+refs.modal.addEventListener('hide.bs.modal', () => {
+  console.log('qwe');
+});
