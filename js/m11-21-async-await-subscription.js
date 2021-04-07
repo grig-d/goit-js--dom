97fe119954718a2b48b364d7c79237ff8a09b5f4
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-04-07 12:41:12 +0300

    subscription done

--- js/m11-21-async-await-subscription.js
+++ js/m11-21-async-await-subscription.js
@@ -1,31 +1,38 @@
-// let promptCounter = 0;
-// let hasSubscribed = false;
-
-// const intervalId = setInterval(() => {
-//   if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubscribed) {
-//     clearInterval(intervalId);
-//     return;
-//   }
-//   console.log('Подпишись на рассылку!');
-//   promptCounter++;
-// }, PROMPT_DELAY);
-
 const modal = new BSN.Modal('#subscriptionModal');
 const refs = {
   modal: document.querySelector('#subscriptionModal'),
-  // closeSecondaryBtnRef: document.querySelector('.js-secondary-close'),
+  closeSecondaryBtnRef: document.querySelector('.js-secondary-close'),
+  subscribeBtnRef: document.querySelector('.btn-primary'),
 };
 
 const PROMPT_DELAY = 1000;
 const MAX_PROMPT_ATTEMPTS = 3;
+let promptCounter = 0;
+let hasSubscribed = false;
 
-setTimeout(() => {
-  modal.show();
-}, PROMPT_DELAY);
+openModal();
 
-// closeSecondaryBtnRef.addEventListener('click', modal.hide);
+refs.closeSecondaryBtnRef.addEventListener('click', () => {
+  modal.hide();
+});
 
 // кастомное событие custom event из библиотеки bootstrap.native
 refs.modal.addEventListener('hide.bs.modal', () => {
-  console.log('qwe');
+  openModal();
 });
+
+refs.subscribeBtnRef.addEventListener('click', () => {
+  hasSubscribed = true;
+  modal.hide();
+});
+
+function openModal() {
+  if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubscribed) {
+    return;
+  }
+  setTimeout(() => {
+    modal.show();
+    promptCounter += 1;
+    console.log(promptCounter);
+  }, PROMPT_DELAY);
+}
