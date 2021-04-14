05d5e5cbe8a968be0475bb7d679d13a519e114b0
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-04-14 20:39:07 +0300

    wip

--- js/m11-21-promise.js
+++ js/m11-21-promise.js
@@ -1,24 +1,23 @@
-// const promise = new Promise((resolve, reject) => {
-//   const canFullfill = Math.random() > 0.5;
+const promise = new Promise((resolve, reject) => {
+  const canFullfill = Math.random() > 0.5;
 
-//   setTimeout(() => {
-//     if (canFullfill) {
-//       resolve('V - Выполнен успешно - передача во внешний код');
-//     }
-//     reject('X - Отклонён - передача во внешний код');
-//   }, 200);
-// });
+  setTimeout(() => {
+    if (canFullfill) {
+      resolve('V - Выполнен успешно - передача во внешний код');
+    }
+    reject('X - Отклонён - передача во внешний код');
+  }, 200);
+});
 
-// console.log(promise);
+promise.then(onFullfilled, onRejected);
 
-// promise.then(onFullfilled, onRejected);
+function onFullfilled(result) {
+  console.log(result);
+}
+function onRejected(error) {
+  console.log(error);
+}
 
-// function onFullfilled(result) {
-//   console.log(result);
-// }
-// function onRejected(error) {
-//   console.log(error);
-// }
 
-/////
+// promisification
 
