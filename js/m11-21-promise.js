9dfb673b261b62bf8bc1e36cdd2716008e671937
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-04-16 19:13:33 +0300

    wip

--- js/m11-21-promise.js
+++ js/m11-21-promise.js
@@ -1,39 +1,38 @@
-const promise = new Promise((resolve, reject) => {
-  const canFullfill = Math.random() > 0.5;
+// const promise = new Promise((resolve, reject) => {
+//   const canFullfill = Math.random() > 0.5;
 
-  setTimeout(() => {
-    if (canFullfill) {
-      resolve('V - Выполнен успешно - передача во внешний код');
-    }
-    reject('X - Отклонён - передача во внешний код');
-  }, 200);
-});
+//   setTimeout(() => {
+//     if (canFullfill) {
+//       resolve('V - Выполнен успешно - передача во внешний код');
+//     }
+//     reject('X - Отклонён - передача во внешний код');
+//   }, 200);
+// });
 
-promise.then(onFullfilled, onRejected);
+// promise.then(onFullfilled, onRejected);
 
-function onFullfilled(result) {
-  console.log(result);
-}
-function onRejected(error) {
-  console.log(error);
-}
+// function onFullfilled(result) {
+//   console.log(result);
+// }
+// function onRejected(error) {
+//   console.log(error);
+// }
+////////
+// // promisification
+// const fetchPokemonById = id => {
+//   return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(r => r.json());
+// };
 
-// promisification
-// "keywords": "#FA842B",
-// "types": "#DDA0DD", // RAL 2003
+// fetchPokemonById(1).then(onFetchSuccess).catch(onFetchError);
 
-const fetchPokemonById = id => {
-  return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(r => r.json());
-};
+// function onFetchSuccess(pokemon) {
+//   console.log('onFetchSuccess -> onFetchSuccess');
+//   console.log(pokemon);
+// }
 
-fetchPokemonById(1).then(onFetchSuccess).catch(onFetchError);
+// function onFetchError(error) {
+//   console.log('onFetchError -> onFetchError');
+//   console.log(error);
+// }
+////////
 
-function onFetchSuccess(pokemon) {
-  console.log('onFetchSuccess -> onFetchSuccess');
-  console.log(pokemon);
-}
-
-function onFetchError(error) {
-  console.log('onFetchError -> onFetchError');
-  console.log(error);
-}
