f2e1f7dc11a5f8354e516ea233af57579c03e58d
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-04-18 03:15:02 +0300

    m11-22 done

--- js/m11-22-racetrack.js
+++ js/m11-22-racetrack.js
@@ -6,6 +6,7 @@ const horses = [
   'Seabiscuit',
 ];
 
+let raceCounter = 0;
 const refs = {
   startBtn: document.querySelector('.js-race-btn'),
   winnerField: document.querySelector('.js-winner'),
@@ -13,23 +14,20 @@ const refs = {
   tableBody: document.querySelector('.js-result-table > tbody'),
 };
 
-refs.startBtn.addEventListener('click', () => {
+refs.startBtn.addEventListener('click', onStart);
+
+function onStart() {
+    raceCounter += 1;
     refs.startBtn.disabled = true;
+    
   // массив промисов .map(run) - сокращенная запись
   const promises = horses.map(run);
 
-  refs.progressField.textContent = 'Заезд начался! Ставки не принимаются!';
-
-  Promise.race(promises).then(({ horse, time }) => {
-    refs.winnerField.textContent = `Победил ${horse}, финишировав за время ${time}`;
-  });
-
-  Promise.all(promises).then(() => {
-      refs.progressField.textContent = 'Заезд окончен, принимаются ставки';
-      refs.startBtn.disabled = false;
-  });
-    
-});
+  updateWinnerField('');
+  updateProgressField('🎬 Заезд начался! Ставки не принимаются!');
+  determineWinner(promises);
+  waitForAll(promises);
+}
 
 // функция запускает одного коня
 function run(horse) {
@@ -43,4 +41,31 @@ function run(horse) {
 
 function getRandomTime(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function determineWinner(horsesPromises) {
+  Promise.race(horsesPromises).then(({ horse, time }) => {
+    updateWinnerField(`🏆 Победил ${horse}, финишировав за время ${time}`);
+    updateResultTable({ horse, time, raceCounter });
+  });
+}
+
+function waitForAll(horsesPromises) {
+  Promise.all(horsesPromises).then(() => {
+    updateProgressField('🏁 Заезд окончен, принимаются ставки');
+    refs.startBtn.disabled = false;
+  });
+}
+
+function updateWinnerField(message) {
+  refs.winnerField.textContent = message;
+}
+
+function updateProgressField(message) {
+  refs.progressField.textContent = message;
+}
+
+function updateResultTable({ horse, time, raceCounter }) {
+  const tr = `<tr><td>${raceCounter}</td><td>${horse}</td><td>${time}</td></tr>`;
+  refs.tableBody.insertAdjacentHTML('beforeend', tr);
 }
