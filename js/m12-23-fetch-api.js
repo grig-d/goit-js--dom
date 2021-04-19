eecf8082645b4a24f2822b757480579d417a5a4b
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-04-19 20:04:18 +0300

    m12-23-fetch-api

--- js/m12-23-fetch-api.js
+++ js/m12-23-fetch-api.js
@@ -1,24 +1,57 @@
+//////// грязный код
+// const templateRef = document.querySelector('#markup').innerHTML.trim();
+// const pokemonCardTemplate = Handlebars.compile(templateRef);
+// const cardContainerRef = document.querySelector('.js-card-container');
+
 // fetch(`https://pokeapi.co/api/v2/pokemon/79`)
 //   .then(response => {
-//     console.log(response);
 //     return response.json();
 //   })
 //   .then(pokemon => {
-//     console.log(pokemon);
-//     document
-//       .querySelector('body')
-//       .insertAdjacentHTML(
-//         'beforeend',
-//         `<div style="text-align: center;"><img src="${pokemon.sprites.front_default}"/><p>${pokemon.name}</p></div>`,
-//       );
+//       const markup = pokemonCardTemplate(pokemon);
+//       cardContainerRef.insertAdjacentHTML('beforeend', markup);
 //   })
 //   .catch(error => console.log(error));
 
+//////// превращаем в чистый код
+const refs = {
+  cardContainer: document.querySelector('.js-card-container'),
+  template: document.querySelector('#markup').innerHTML.trim(),
+  searchForm: document.querySelector('.js-search-form'),
+  btn: document.querySelector('.btn-primary'),
+};
 
-const templateObject = document.querySelector('#markup').innerHTML.trim();
-console.log(templateObject);
-const markup = Handlebars.compile(templateObject);
-document.querySelector('body').insertAdjacentHTML('beforeend', markup);
-console.log(markup);
+refs.searchForm.addEventListener('submit', onSearch);
 
-// 0-50-00
+const pokemonCardTemplate = Handlebars.compile(refs.template);
+
+function renderPokemonCard(pokemon) {
+  const markup = pokemonCardTemplate(pokemon);
+  refs.cardContainer.innerHTML = markup;
+}
+
+function fetchPokemonById(pokemonId) {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`).then(
+    response => {
+      return response.json();
+    },
+  );
+}
+
+function onFetchError(error) {
+  alert('Упс, что-то пошло не так и мы не нашли вашего покемона');
+}
+
+function onSearch(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const searchQuery = form.elements.query.value;
+  if (searchQuery) {
+    fetchPokemonById(searchQuery)
+      .then(renderPokemonCard)
+      .catch(onFetchError)
+      .finally(form.reset());
+  }
+}
+
+// 1-29-22
